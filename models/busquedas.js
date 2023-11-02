062309525c8856fe3c20d5af0cb08ea0a5c5ce4e
GeovannyTS <yowtecum@gmail.com>
2023-11-02 10:59:55 -0600

    Http request structure

--- models/busquedas.js
+++ models/busquedas.js
@@ -4,19 +4,41 @@ class Busquedas {
 
     historial = ['Guatemala', 'Mexico', 'Bogota'];
 
-    constructor(){
+    constructor() {
         // Leer la bd si existe
     }
+
+    get paramsMapbox() {
+        return {
+            'access_token': '',
+            'limit': 5,
+            'lenguage': 'es',
+        }   
+    }
+
     // Metodo para buscar una ciudad 
+    async ciudad(lugar = '') {
 
-    async ciudad ( lugar = '' ){
+        try {
+            // Peticion HTTP con axios a la api
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox,
+            });
 
-        // Peticion HTTP con axios a la api
-        const respuesta = await axios.get('https://reqres.in/api/users?page=2')
-        console.log(respuesta.data);
+            const respuesta = await instance.get();
+            console.log(respuesta.data);
 
-        // Regresa todos los lugares
-        return []; 
+            // Regresa todos los lugares
+            return [];
+
+        } catch (error) {
+            return [];
+        }
+
+
+
+
     }
 
 }
